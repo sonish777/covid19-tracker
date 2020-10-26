d44e155e60aa1f4951a4bb2b7d714d72f2d20866
Sonish Maharjan <sonishmaharjan1@gmail.com>
2020-10-26 19:42:26 +0545

    Updates

---
+++ precache-manifest.a17216a162ca9297ba816af48ba8f65d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce4be82d690e182587f2f6089b0a8d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cced1f97fb809c93511",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "f14db8e215065a296398",
+    "url": "/static/css/main.9240c6e3.chunk.css"
+  },
+  {
+    "revision": "3cced1f97fb809c93511",
+    "url": "/static/js/2.7353fae8.chunk.js"
+  },
+  {
+    "revision": "0b110a718d0a8fc6b16693cbaab585fa",
+    "url": "/static/js/2.7353fae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14db8e215065a296398",
+    "url": "/static/js/main.abcb18ac.chunk.js"
+  },
+  {
+    "revision": "ad3d3f1d34a73b724b01",
+    "url": "/static/js/runtime-main.b1bfe79c.js"
+  }
+]);
